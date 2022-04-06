aa9b903921c8ccad4128e98c735ad71f2db57793
wangjia <wangjia_1919@163.com>
2022-04-06 22:38:58 +0800

    fix icons

---
+++ src/main/kotlin/com/github/nineswordsmonster/crispylamp/NineIcons.kt
@@ -0,0 +1,10 @@
+package com.github.nineswordsmonster.crispylamp
+
+import com.intellij.openapi.util.IconLoader
+
+object NineIcons {
+    @JvmField
+    val Action = IconLoader.getIcon("/icons/soup.png", javaClass)
+    @JvmField
+    val ActionGroup = IconLoader.getIcon("/icons/soup.svg", javaClass)
+}
